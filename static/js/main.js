c92f543fbd89ca7c8b0983e6e29287e5b4085732
TriLD <triledinh102097@gmail.com>
2023-03-31 18:51:11 +0700

    add counter and music

--- static/js/main.js
+++ static/js/main.js
@@ -29,3 +29,24 @@ setInterval(function name(params) {
         }
     }
 }, 400)
+
+
+/* ========================= */
+var track = document.getElementById('track');
+var controlBtn = document.getElementById('play-pause');
+function playPause() {
+    if (track.paused || controlBtn.className == "play") {
+        track.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn.className = "pause";
+    } else { 
+        track.pause();
+         //controlBtn.textContent = "Play";
+        controlBtn.className = "play";
+    }
+}
+controlBtn.addEventListener("click", playPause);
+track.addEventListener("ended", function() {
+    controlBtn.className = "play";
+});
+
